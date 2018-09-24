35f4597861ad42195e52d3f871db85b54a1ffaed
Jasper <leeu1606@student.nhl.nl>
2018-09-24 13:14:41 +0200

    Graphs+Algorithm
    AAA

---
+++ AmazonSimulator VS/wwwroot/moving/Graph.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazonSimulator VS.wwwroot.moving
+{
+    public class Graph
+{
+}
+}
